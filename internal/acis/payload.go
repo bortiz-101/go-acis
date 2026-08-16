20fb25788349d419df0dd3921839ff12c056d7a3
bortiz-101 <bortiz101@hotmail.com>
2026-08-15 20:38:31 -0500

    adds stndatarequest struct

--- internal/acis/payload.go
+++ internal/acis/payload.go
@@ -13,16 +13,86 @@ const (
 
 type StnMetaRequest struct {
 	SIDS    []string    `json:"sids"`
-	COUNTY  int         `json:"county"`
+	County  int         `json:"county"`
 	CLIMDIV int         `json:"climdiv"`
 	CWA     string      `json:"cwa"`
-	BASIN   int         `json:"basin"`
-	STATE   string      `json:"state"`
+	Basin   int         `json:"basin"`
+	State   string      `json:"state"`
 	BBOX    float64     `json:"bbox"`
-	META    *[]string   `json:"meta,omitempty"`
-	ELEMS   *[]int      `json:"elems,omitempty"`
-	SDATE   *string     `json:"sdate,omitempty"`
-	EDATE   *string     `json:"edate,omitempty"`
-	DATE    *string     `json:"date,omitempty"`
-	OUTPUT  *OutputType `json:"output,omitempty"`
+	Meta    *[]string   `json:"meta,omitempty"`
+	Elems   *[]int      `json:"elems,omitempty"`
+	SDate   *string     `json:"sdate,omitempty"`
+	EDate   *string     `json:"edate,omitempty"`
+	Date    *string     `json:"date,omitempty"`
+	Output  *OutputType `json:"output,omitempty"`
+}
+
+type Interval struct {
+	Shortcut *string
+	Values   []int
+}
+
+type Reduce struct {
+	Code          *string
+	Reduction     *string
+	Add           []string
+	N             *int
+	RunMaxMissing *int
+}
+
+type SeasonStart struct {
+	Date   *string
+	Values []int
+}
+
+type Normal struct {
+	Value *int
+	Code  *string
+}
+
+type SummaryObject struct {
+	Reduce        string
+	Add           []string
+	N             *int
+	RunMaxMissing *int
+}
+
+type Summary struct {
+	Code   *string
+	Codes  []string
+	Object *SummaryObject
+}
+
+type GroupBy struct {
+	Shortcut *string
+	Values   []string
+}
+
+type Element struct {
+	Name *string  `json:"name,omitempty"`
+	VX   *float64 `json:"vX,omitempty"`
+	VN   *int     `json:"vN,omitempty"`
+	Base *float64 `json:"base,omitempty"`
+
+	Interval    Interval    `json:"interval,omitempty"`
+	Duration    *string     `json:"duration,omitempty"`
+	Reduce      Reduce      `json:"reduce,omitempty"`
+	SeasonStart SeasonStart `json:"season_start,omitempty"`
+	Add         []string    `json:"add,omitempty"`
+	Normal      Normal      `json:"normal,omitempty"`
+	MaxMissing  *int        `json:"maxmissing,omitempty"`
+	Prec        *int        `json:"prec,omitemtpy"`
+	SMRY        Summary     `json:"smry,omitempty"`
+	SMRYONLY    *int        `json:"smry_only,omitempty"`
+	GroupBy     GroupBy     `json:"groupby,omitempty"`
+}
+
+type StnDataRequest struct {
+	SID    string      `json:"sid"`
+	SDate  string      `json:"sdate"`
+	EDate  string      `json:"edate"`
+	Date   string      `json:"date"`
+	Elems  []Element   `json:"elems"`
+	Meta   *[]string   `json:"meta,omitempty"`
+	Output *OutputType `json:"output,omitempty"`
 }
