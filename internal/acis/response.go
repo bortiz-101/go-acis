710aa77f2103036af6e147f63a1eea305d6de3cc
bortiz-101 <bortiz101@hotmail.com>
2026-08-16 12:38:25 -0500

    adds stationmeta response struct

--- internal/acis/response.go
+++ internal/acis/response.go
@@ -1,1 +1,28 @@
 package acis
+
+type DateRange struct {
+	Start *string
+	End   *string
+}
+
+type Coordinates struct {
+	Longitude float64
+	Latitude  float64
+}
+
+type StationMeta struct {
+	Name           string       `json:"name,omitempty"`
+	State          string       `json:"state,omitempty"`
+	SIDS           []string     `json:"sids,omitempty"`
+	SIDDATES       []DateRange  `json:"sid_dates,omitempty"`
+	LL             *Coordinates `json:"ll,omitempty"`
+	Elevation      float64      `json:"elev,omitempty"`
+	UID            string       `json:"uid,omitempty"`
+	County         string       `json:"county,omitempty"`
+	CLIMDIV        string       `json:"climdiv,omitempty"`
+	ValidDataRange []DateRange  `json:"valid_daterange,omitempty"`
+}
+
+type StnMetaResonpse struct {
+	Meta []StationMeta `json:"meta"`
+}
