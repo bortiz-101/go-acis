8654997b219318486920882d0bccc0bc241e3685
nplace <nicholas.place@caci.com>
2026-08-17 11:05:02 -0400

    updated router

--- internal/http/router.go
+++ internal/http/router.go
@@ -1,14 +1,18 @@
 package apihttp
 
 import (
+	"github.com/bortiz-101/go-acis/internal/acis"
 	"github.com/bortiz-101/go-acis/internal/http/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-// define all of our routes here
-func CreateRouter() *gin.Engine {
+// router now needs ACIS client to be created
+func CreateRouter(acisClient *acis.Client) *gin.Engine {
 	router := gin.Default()
 
+	// gin router just marries endpoint to handler func
 	router.GET("/health", handlers.Health)
+	router.POST("api/stn-data", handlers.StnData(acisClient))
+
 	return router
 }
